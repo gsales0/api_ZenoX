3f2c15ac4b8b16612592da4af5f34d4e0b26009f
Sales <salesg3.fatorsistemas@gmail.com>
2026-05-24 19:35:44 -0300

    :wrench: Em Desenvolvimento: Relatórios

--- routes/reports/listagem.js
+++ routes/reports/listagem.js
@@ -11,50 +11,41 @@ const fonts = {
     }
 };
 
-pdfmake.setUrlAccessPolicy(() => true);
-pdfmake.setLocalAccessPolicy(() => true);
+const createPdf = function(entidade){
 
-app.post('/reports/listagem', async(req, res) => {
-
-    let ID_ENTIDADE = jwt.verify(req.headers.x_session, process.env.XKEY).ID_ENTIDADE
-
-    let [data] = await con.promise().query('SELECT * FROM ENTIDADES WHERE ID_ENTIDADE = ?', ID_ENTIDADE)
-
-    let docDefinition = {
+    return config = {
         defaultStyle: { font: 'Helvetica' },
         pageSize: 'A4',
         pageMargins: [ 40, 130, 40, 60 ],
 
         header: function(currentPage, pageCount) {
             return {
-                margin: [40, 30, 40, 0], // [esquerda, cima, direita, baixo]
+                margin: [40, 30, 40, 0],
                 stack: [
                     {
                         columns: [
-                            // LADO ESQUERDO: A LOGO
                             {
-                                image: data[0].ANEXO,
-                                width: 90, // Largura fixa para não desalinhar
+                                image: entidade[0].ANEXO,
+                                width: 90,
                             },
-                            // LADO DIREITO: INFO DA ENTIDADE
                             {
                                 stack: [
-                                    { text: data[0].DS_ENTIDADE, fontSize: 12, bold: true,margin: [0, 0, 0, 5] },
-                                    { text: `${data[0].DS_ENDERECO}, ${data[0].NUM_ENDERECO} - ${data[0].DS_CIDADE}`, fontSize: 9, color: '#444',margin: [0, 0, 0, 5] },
-                                    { text: `CNPJ: ${data[0].CNPJ}`, fontSize: 9, color: '#444' },
+                                    { text: entidade[0].DS_ENTIDADE, fontSize: 16, bold: true,margin: [0, 0, 0, 5] },
+                                    { text: `${entidade[0].DS_ENDERECO}, ${entidade[0].NUM_ENDERECO} - ${entidade[0].DS_CIDADE}`, fontSize: 9, color: '#444',margin: [0, 0, 0, 5] },
+                                    { text: `CNPJ: ${entidade[0].CNPJ}`, fontSize: 9, color: '#444' },
+                                    { text: "Relatório de Listagem", fontSize: 14, bold: true, margin: [0, 25, 0, 0], color: "red"}
                                 ],
                                 alignment: 'right',
-                                margin: [0, 5, 0, 0] // Ajuste fino para alinhar com a logo
+                                margin: [0, 5, 0, 0]
                             }
                         ]
                     },
-                    // LINHA DIVISÓRIA (O toque de classe)
                     {
                         canvas: [
                             { 
                                 type: 'line', 
                                 x1: 0, y1: 10, 
-                                x2: 515, y2: 10, // 515 é a largura útil do A4 (595 - 80 de margens)
+                                x2: 515, y2: 10,
                                 lineWidth: 0.5, 
                                 lineColor: '#bcbcbc' 
                             }
@@ -64,11 +55,41 @@ app.post('/reports/listagem', async(req, res) => {
             }
         }
     }
+}
+
+pdfmake.setUrlAccessPolicy(() => true);
+pdfmake.setLocalAccessPolicy(() => true);
+
+app.post('/reports/listagem/:table', async(req, res) => {
+
+    let ID_ENTIDADE = jwt.verify(req.headers.x_session, process.env.XKEY).ID_ENTIDADE
+
+    let [data] = await con.promise().query('SELECT * FROM ENTIDADES WHERE ID_ENTIDADE = ?', ID_ENTIDADE)
+
+    let columns = Object.keys(req.body)
+
+    let reportColumns = columns.map(i => {
+        return { text: i, bold: true, fillColor: '#f3f4f6' }
+    })
+
+    let [dados] = await con.promise().query(`SELECT ${columns.join(', ')} FROM ${req.params.table} WHERE ID_ENTIDADE = ?`, ID_ENTIDADE)
+
+
+    let tratados = []
+    dados.forEach(i => {
+        let row = []
+        for(let x of columns){
+            row.push(i[x])
+        }
+        tratados.push(row)
+    })
+
+    let docDefinition = createPdf(data)
 
     pdfmake.addFonts(fonts)
 
     pdfmake.createPdf(docDefinition).getBase64().then((data) => {
-        // O pdfmake devolve a string limpa. Nós só colamos o cabeçalho do navegador:
+
         const base64String = 'data:application/pdf;base64,' + data;
         res.send({ success: true, file: base64String });
         
